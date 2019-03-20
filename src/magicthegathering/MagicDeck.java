852d66587221005edbc74c4eb2d520f51a95e5cb
campPW <camp.pmw@gmail.com>
2019-03-20 15:00:07 -0700

    Added method to determine and set probable deck format.

--- src/magicthegathering/MagicDeck.java
+++ src/magicthegathering/MagicDeck.java
@@ -5,11 +5,12 @@ public class MagicDeck {
     private ArrayList<MagicCard> cards = new ArrayList<MagicCard>();
     private int deckSize;
     private double averageCMC;
-
+    private String deckFormat;
     public MagicDeck(ArrayList<MagicCard> magicCards) {
         this.cards = magicCards;
         this.setDeckSize();
         this.setAverageCMC();
+        this.setDeckFormat(deckSize);
     }
     public ArrayList<MagicCard> getCards() {
         return this.cards;
@@ -36,5 +37,13 @@ public class MagicDeck {
         String avgStr = Double.toString(average).substring(0,3);
         this.averageCMC = Double.parseDouble(avgStr);
     }
-    }
+    private void setDeckFormat(int deckSize){
+        if(deckSize >= 50)
+            this.deckFormat = "Commander";
+        else if(deckSize >= 30 && deckSize <= 45)
+            this.deckFormat = "Constructed";
+        else
+            this.deckFormat = "Limited";
+        }
+}
 
